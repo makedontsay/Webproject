abec6fc9b324ea783d99021e8ab1859f271b14a2
makedontsay <50293563+makedontsay@users.noreply.github.com>
2019-05-07 04:09:10 +0700

    add navbar to cartphp

--- webfinal/cart.php
+++ webfinal/cart.php
@@ -59,12 +59,32 @@ function pre_r($array){
 <html>
 	<head>
 		<title>Shopping Cart</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="css/cart.css"/>
 
 	</head>
 	<body>
-		<div class="home-welcome-text" style="background-image: url(image/homebackgrndimgss.jpg); height: 550px; ">
+		<div class="sidenav test">
+	<img class="center-block" src="pic\logo.png" width="200" height="200" alt="logoimg">
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+
+<ul>
+  <li><button type="button" class="btn btn-outline-info">Log Out</button></li>
+  <li><a class="username">test111</a></li>
+  <li><a>Welcome , </a></li>
+  <li><img src="pic\logo.png" width="50" height="50" alt="profilepic"></li>
+  <li class="topic1"><a>market order</a></li>
+</ul>
+		
+		<img class="image1" src="image/homebackgrndimgss.jpg" alt="img">
+		<div class="home-welcome-text">
 		</div>
 		<div class="container">
 		<?php
