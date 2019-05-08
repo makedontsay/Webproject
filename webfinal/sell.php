bec79018ff3e907fce5e5744a943ae883768f225
semidr <39444525+semidr@users.noreply.github.com>
2019-05-09 00:39:55 +0700

    s

--- webfinal/sell.php
+++ webfinal/sell.php
@@ -59,11 +59,11 @@ session_start();
                        <div class="card mb-3" style="max-width: 540px;">
   <div class="row no-gutters">
     <div class="col-md-4">
-      <img src= <?php echo "pic/".$imgfile ?> class="card-img" alt="...">
+      <img src= "pic/cart.jpg" class="card-img" alt="...">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h1 class="card-title" id="username"><?php echo $username ?></h1>
+        <h1 class="card-title" id="username">TRADE MARKET</h1>
         <h5 class="card-title" id="email"><?php echo $email ?></h5>
       </div>
     </div>
